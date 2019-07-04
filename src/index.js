38dccd8e0005246bd8185d29d04cbfe53bdd87b1
sandy <sandytian2000@hotmail.com>
2019-07-04 19:49:21 -0400

    connected socket

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDiriectionPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDiriectionPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('new websocket connection');
+})
+
+server.listen(port, () => {
   console.log(`Service is up on port ${port}`);
 })

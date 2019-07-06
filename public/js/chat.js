250ab8d644ff64a66d4362c735c255bb45dcc3b7
sandy <sandytian2000@hotmail.com>
2019-07-06 09:39:32 -0400

    no more spamming

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,22 @@
 const socket = io();
 
-document.querySelector('#welcome').addEventListener('submit', (e) => {
+const $messageForm = document.querySelector('#messageForm');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  $messageFormButton.setAttribute('disabled', 'disabled');
+  // disable
   const message = e.target.elements.message.value;
+
   socket.emit('sendMessage', message, (error) => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+    // enable
+
     if (error) {
       return console.log(error)
     }
